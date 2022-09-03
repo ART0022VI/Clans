8fa22e2bb6dd1e9ceb9b519d38fb7919f3158329
ART0022VI <artem.jidov@gmail.com>
2022-09-03 18:04:49 +0700

    setup

---
+++ Clans/Main.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Qurre;
+using Qurre.API;
+using Qurre.API.Events;
+using Qurre.Events;
+using Player = Qurre.Events.Player;
+namespace Clans
+{
+    internal class Main : Plugin
+    {
+        public List<string> plids = new List<string>()
+        {
+            "Test" // тег клана
+        };
+        public override void Disable()
+        {
+
+        }
+
+        public override void Enable()
+        {
+            Player.Join += OnJoin;
+        }
+        public void OnJoin(JoinEvent ev)
+        {
+            if(ev.Player.Nickname.Contains("[") && ev.Player.Nickname.Contains("]"))
+            {
+                string[] pre = ev.Player.Nickname.Split('[', ']');
+                string tag = pre[1];
+                Log.Info(tag);
+                if (plids.Contains(tag))
+                {
+                    Log.Info("Есть");
+                }
+                else
+                {
+                    plids.Add(tag);
+                    Log.Info("добавил");
+                    foreach(var p in plids)
+                    {
+                        Log.Info(p);
+                    }
+                }
+            }
+        }
+    }
+}
